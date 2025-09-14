41c1dc100a153561ff4f53d9ebb16c24ffc50369
AAEStudy <zach8917@gmail.com>
2025-09-14 04:29:21 -0400

    added the script

--- Assignment_02_Data_Visualization/Assignment_02.R
+++ Assignment_02_Data_Visualization/Assignment_02.R
@@ -1,1 +1,38 @@
+https://rstudioclass.blogspot.com/2025/09/assignment-2.html
+# When we run:
+#   assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+#   
+#   myMean <- function(assignment2) {
+#     
+#     return(sum(assignment) / length(someData))
+#     
+#   }
+#   
+#   myMean(assignment2)
+#   
+#   It returns the error:
+#     
+#     myMean(assignment2)
+#   
+#   Error in myMean(assignment2) : object 'assignment' not found
+#   
+#   This occurs because the function refers to 'assignment' and 'someData,' which are names that don't exist instead of the parameter 'assignment2'. The corrected function would look like:
+# 
+# myMean <- function(assignment2) {
+# 
+#   return(sum(assignment2) / length(assignment2))
+# 
+# }
+# 
+# myMean(assignment2)
+# 
+# The corrected function outputs the mean of the list, which is 19.25. 
+# 
 
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+myMean <- function(assignment2) {
+  return(sum(assignment2) / length(assignment2))
+}
+
+myMean(assignment2)
