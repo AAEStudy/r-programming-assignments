8378c73b1446e3ce4809d6eb87021b0c4c05f0e8
AAEStudy <zach8917@gmail.com>
2025-11-03 07:54:12 -0500

    Add placeholder function for running mediation models
    This function serves as a placeholder for future mediation analyses using Python's pyprocessmacro.

---
+++ Assignment_10_PackageSkeleton/R/run_all_mediations.R
@@ -0,0 +1,10 @@
+#' Run All Mediation Models (Placeholder)
+#'
+#' This function will eventually run all combinations of mediation analyses
+#' using Python's pyprocessmacro.
+#'
+#' @export
+run_all_mediations <- function() {
+  message("AutomatedMediation package is set up correctly!")
+  message("Later, this will call Python's PROCESS macro.")
+}
